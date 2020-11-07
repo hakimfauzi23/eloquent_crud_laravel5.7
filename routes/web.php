b7ca6473219d457e4c0e9db82a79b93d2674667c
Hanif Fauzi Hakim <haniffauzihakim4049@gmail.com>
2020-11-07 14:00:16 +0700

    #21 CRUD : dengan menggunakan Eloquent

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/pegawai', 'PegawaiController@index');
+Route::get('/pegawai/tambah', 'PegawaiController@tambah');
+Route::post('/pegawai/store', 'PegawaiController@store');
+Route::get('/pegawai/edit/{id}', 'PegawaiController@edit');
+Route::put('/pegawai/update/{id}', 'PegawaiController@update');
+Route::get('/pegawai/hapus/{id}', 'PegawaiController@delete');
